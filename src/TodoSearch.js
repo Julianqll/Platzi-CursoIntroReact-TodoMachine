626e5170ed25b1aec9b0fc6db5c811560b1a0c6a
Julian Quispe <julianmql@hotmail.com>
2023-01-26 18:19:32 -0500

    feat: state management added - lesson#9 completed

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -3,17 +3,24 @@ import './TodoSearch.css';
 
 function TodoSearch(){
 
+    //El useState devuelve un array con 2 pos [state, func setState]
+    const [searchValue, setSearchValue] = React.useState('');
+
     const onSearchValueChanged = (event) => {
         console.log(event.target.value);
+        setSearchValue(event.target.value);
     };
 
-    return(
+    //React no puede renderizar varias etiquetas para un solo component, por eso usamos un array/fragment
+    return[
         <input 
             className="TodoSearch" 
-            placeholder="Cebolla"
+            placeholder="Busca una tarea"
+            value={searchValue}
             onChange={onSearchValueChanged}
-        />
-    );
+        />,
+        <p>{searchValue}</p>
+    ];
 }
 
 export { TodoSearch };
