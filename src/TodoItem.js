423093b15285f21e13521ef55c7be52dc1fab342
Julian Quispe <julianmql@hotmail.com>
2023-01-26 17:52:31 -0500

    feat: event managment added - lesson#8 completed

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,21 +1,37 @@
 import React from "react";
 import './TodoItem.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheck} from '@fortawesome/free-solid-svg-icons';
+import { faCheck, faTrash} from '@fortawesome/free-solid-svg-icons';
 
 
 
 function TodoItem(props) {
+
+    const onComplete = () => {
+      alert("Ya completaste el todo " + props.text);
+    };
+
+    const onDelete = () => {
+      alert("Borraste el todo " + props.text);
+    };
+
+
     return (
       <li className="TodoItem">
-        <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>
+        <span 
+          className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
+          onClick={onComplete}
+        >
           <FontAwesomeIcon icon={faCheck} />
         </span>
         <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
           {props.text}
         </p>
-        <span className="Icon Icon-delete">
-          <i class="fa fa-trash"></i>
+        <span 
+          className="Icon Icon-delete"
+          onClick={onDelete}
+        >
+          <FontAwesomeIcon icon={faTrash} />
         </span>
       </li>
     );
