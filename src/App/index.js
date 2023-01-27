8947af13ae8d51857a141c92257e435312922d1d
Julian Quispe <julianmql@hotmail.com>
2023-01-26 22:49:37 -0500

    fear: customHook created - lesson#14 completed

--- src/App/index.js
+++ src/App/index.js
@@ -10,25 +10,39 @@ import { AppUI } from "./AppUi";
 //  {text: "Entrenar", completed: false},
 //]
 
+function useLocalStorage(itemName, initialValue) {
+
+  const localStorageItem = localStorage.getItem(itemName);
+  let parsedItem;
+
+  if (!localStorageItem){
+    localStorage.setItem(itemName, JSON.stringify(initialValue));
+    parsedItem = initialValue;
+  }
+  else{
+    parsedItem = JSON.parse(localStorageItem);
+  }
+
+  const [item, setItem] = React.useState(parsedItem);
+
+
+  const saveItem = (newItem) => {
+    const stringifiedItem = JSON.stringify(newItem);
+    localStorage.setItem(itemName, stringifiedItem);
+    setItem(newItem);
+  };
+
+  return [item, saveItem];
+
+}
+
 
 function App() {
 
-  const localStorageTodos = localStorage.getItem('TODOS_V1');
-  let parsedTodos;
-
-  if (!localStorageTodos){
-    localStorage.setItem('TODOS_V1', JSON.stringify([]));
-    parsedTodos = [];
-
-  }
-  else{
-    parsedTodos = JSON.parse(localStorageTodos);
-  }
+  const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
 
   //El useState devuelve un array con 2 pos [state, func setState]
   const [searchValue, setSearchValue] = React.useState('');
-
-  const [todos, setTodos] = React.useState(parsedTodos);
 
   const completedTodos = todos.filter(todo => !!todo.completed).length;
   const totalTodos = todos.length;
@@ -45,15 +59,6 @@ function App() {
       return todoText.includes(searchText);
     });
   }
-
-
-  const saveTodos = (newTodos) => {
-    const stringifiedTodos = JSON.stringify(newTodos);
-    localStorage.setItem('TODOS_V1', stringifiedTodos);
-    setTodos(newTodos);
-  };
-
-
 
   const completeTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text === text);
